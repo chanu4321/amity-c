58884fec84eceda54c4426c31a8195681abdf16b
chanu4321 <chanugupta2012@gmail.com>
2023-09-03 10:37:32 +0530

    added string operations code

---
+++ string_operations.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+/*
+int main() {
+    int count=0, a;
+    char s[10];
+    scanf(" %s",&s);
+    for (int i=0; s[i]!='\0'; i++) {
+        count++;
+    }
+    a=strlen(s);
+    printf("lengh of given string %s is %d \n", s, count);
+    printf("%d",a);
+    return 0;
+}
+
+
+int main() {
+    int count=0, a;
+    char s1[10], s2[10];
+    scanf(" %s",&s1);
+    // for (int i=0; s1[i] != '\0';i++){
+    //     s2[i]=s1[i];
+    // }
+    strcpy(s2,s1);
+    printf("str 2 is %s", s2);
+    return 0;
+}
+*/
