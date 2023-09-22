2a1befa8402866e99efc8979378c18b02fa00e4d
chanu4321 <chanugupta2012@gmail.com>
2023-09-23 00:16:49 +0530

    Insertion Done

--- Simple_LL.c
+++ Simple_LL.c
@@ -7,16 +7,14 @@ struct node
     struct node *next;
 };
 
-void LL_insert()
-{
-}
 
 int main()
 {
     int ch; char ins;
     struct node *start = NULL;
-    struct node *nn;
-    int d;
+    struct node *nn,*temp;
+    int d,k,pos;
+    while (1) {
     printf("Operations on Unsorted Linked List \n \n Press 1 to Insert \n Press 2 to Delete \n Press 3 to Search \n Press 4 to Traverse(Print) \n Press 5 to exit \n");
     scanf("%d", &ch);
     if (ch == 1)
@@ -24,7 +22,6 @@ int main()
         printf(" A: Insert at start \n B: Insert at end \n C: Insert at location \n");
         fflush(stdin);
         ins=getchar();
-        printf("%c",ins);
         switch (ins)
         {
         case 'A':
@@ -33,14 +30,47 @@ int main()
             nn->data = d;
             nn->next = start;
             start = nn;
-
-            printf("element is insetred at the beg : %d",nn->data);
+            printf("element is inserted at the beg : %d \n",nn->data);
+            break;
+        
+        case 'B':
+            nn = (struct node *)malloc(sizeof(struct node));
+            scanf("%d", &d);
+            nn->data = d;
+            nn->next = NULL;
+            if (start==NULL) {
+                start=nn;
+            }
+            else {
+                for(temp=start;temp->next!=NULL;temp=temp->next) {}
+            }
+            temp->next = nn;
+            printf("element is inserted at the end : %d \n",nn->data);
+            break;
+        
+        case 'C':
+            scanf("%d", &d);
+            scanf("%d",&pos);
+            for(temp=start,k=0 ; k<pos ; temp=temp->next) {
+                if(temp==NULL) {
+                    printf("nodes less than position");
+                    break;
+                }
+                k++;
+            }
+            nn = (struct node *)malloc(sizeof(struct node));
+            nn->data = d;
+            nn->next = temp->next;
+            temp->next=nn;
+            printf("element is inserted at the given location : %d \n",nn->data);
+            break;
         }
-    }
+        }
+    
     else if (ch == 2)
     {
-        printf(" A: Delete at start \n B: Delete at end \n C: Delete at location \n");
+        printf(" A: Delete using location \n B: Delete using data value \n ");
     }
-
+    }
     return 0;
 }
